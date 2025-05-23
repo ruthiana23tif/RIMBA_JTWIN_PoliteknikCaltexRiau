219c58356ced4d52d1ec952a71a06c9e30f50bd4
ruthiana23tif <ruthiana23ti@mahasiswa.pcr.ac.id>
2025-05-24 00:46:23 +0700

    sensitive case pada skincare file

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./assets/tailwind.css";
 import { Routes, Route } from "react-router-dom";
 const Dashboard = React.lazy(() => import("./pages/Dashboard"));
 import Makeup from "./pages/Makeup";
-import Skincare from "./pages/skincare";
+import Skincare from "./pages/Skincare";
 import BestSeller from "./pages/BestSeller";
 import NotFound from "./pages/ErrorPage";
 import MainLayout from "./layouts/MainLayout";
