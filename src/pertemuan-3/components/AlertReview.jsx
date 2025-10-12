8b3d6f653e703753047ac4795e88c3b5c19cdeb5
ruthiana23tif <ruthiana23ti@mahasiswa.pcr.ac.id>
2025-10-12 14:39:31 +0700

    perubah

--- src/pertemuan-3/components/AlertReview.jsx
+++
@@ -1,9 +0,0 @@
-export default function AlertReview({ error, message }) {
-    return (
-      error && (
-        <div className="mt-2 p-2 bg-red-100 border-l-4 border-red-500 text-red-700">
-          <p className="text-sm">{message}</p>
-        </div>
-      )
-    );
-  }
